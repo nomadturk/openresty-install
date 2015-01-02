5f1947c87ff1bb7814fb80215c3c51abb7344d2a
nomadturk <az@cokh.net>
2015-01-03 01:24:54 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get -y --force-yes install screen 
-/usr/bin/screen -m -S LOMPInstaller bash -c 'wget --no-check-certificate https://raw.githubusercontent.com/nomadturk/openresty-install/master/inst.sh; exec bash inst.sh'
-echo $(tput setaf 1)The installer is now running in a screen. You can detach or if your connection to ever drop you can reconnect to this screen with the command:$(tput setaf 2) screen -r LOMPInstaller$(tput sgr0)&>> /dev/null 
+wget --no-check-certificate https://raw.githubusercontent.com/nomadturk/openresty-install/master/inst.sh
+echo $(tput setaf 1)The installer is now gonna run in a screen. You can detach or if your connection to ever drop you can reconnect to this screen with the command:$(tput setaf 2) screen -RR $(tput sgr0)&>> /dev/null 
 sleep 5 
+screen -mS Install bash -c "bash inst.sh"
 exit 
