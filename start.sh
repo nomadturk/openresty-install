e216985a001a2cac81aac3440d297c3d68d7685e
M. Omer Golgeli <az@cokh.net>
2015-01-03 19:26:39 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ apt-get -y --force-yes install screen
 wget --no-check-certificate https://raw.githubusercontent.com/nomadturk/openresty-install/master/inst.sh
 echo $(tput setaf 1)The installer is now gonna run in a screen. You can detach or if your connection to ever drop you can reconnect to this screen with the command:$(tput setaf 2) screen -RR $(tput sgr0)&>> /dev/null 
 sleep 5 
-screen -mS Install bash -c "bash inst.sh"
+screen -dmS Install bash -c "bash inst.sh"
 exit 
