bc8be9e0e196d771f6543bebe6acb4b7c72ec8a1
nomadturk <az@cokh.net>
2015-01-03 11:06:01 +0200

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -455,8 +455,11 @@ apt-get -y --force-yes install python-software-properties &>> /dev/null
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db &>> /dev/null
 add-apt-repository 'deb http://lon1.mirrors.digitalocean.com/mariadb/repo/10.0/debian wheezy main'
 apt-get update &>> /dev/null
+# End timer, we do not want mysql password screen to mess up with our resulting time now, do we?
+END=$(date +%s)
 apt-get -y --force-yes install mariadb-server
-
+# Start timer again.
+START2=$(date +%s)
 show_progress "Since I feel lazy, we'll get the Php5.5 from DotDeb..."
 ## DotDeb Php 5.5, 
 add-apt-repository 'deb http://packages.dotdeb.org wheezy all'
@@ -512,10 +515,13 @@ logrotate -f -v /etc/logrotate.d/nginx
 
 show_progress "Done and done... Enjoy it. All is ready to go."
 
-END=$(date +%s)
-DIFF=$(( $END - $START ))
+END2=$(date +%s)
+DIFF1=$(( $END - $START ))
+DIFF2=$(( $END2 - $START2 ))
+DIFF=$(( $DIFF1 + $DIFF2 ))
 echo Hurray! In mere $(($DIFF / 3600 )) hours $(($DIFF / 60 )) minutes and $(($DIFF % 60 )) seconds all is finished! Congrats dude... >>Time.log
 show_progress_info "$(cat Time.log)"
+show_progress_info "This is also excluding the time you spent at MariaDB Password page."
 #echo "Hurray! In mere "$(($DIFF / 3600 ))"" hours "$(($DIFF / 60 ))" minutes and "$(($DIFF % 60 ))" seconds all is finished! Congrats dude..." >>Time.log
 read -s -n 1 any_key | show_progress_info "Press a key to exit now..."
 exit
