5069de695c94dd65caa3b35b2788c72d1587a54c
M. Omer Golgeli <az@cokh.net>
2015-01-03 23:56:15 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,18 +8,18 @@ wget --no-check-certificate https://raw.githubusercontent.com/nomadturk/openrest
 
 
 # Some systems problems with locales. So lets try to add them just to run smoother.
-LANGUAGE="en_US.UTF-8"
-LANG="en_US.UTF-8"
-LC_ALL="en_US.UTF-8"
-LC_COLLATE="en_US.UTF-8"
-LC_CTYPE="en_US.UTF-8"
-LC_MESSAGES="en_US.UTF-8"
-export LANGUAGE="en_US.UTF-8"
-export LANG="en_US.UTF-8"
-export LC_ALL="en_US.UTF-8"
-export LC_COLLATE="en_US.UTF-8"
-export LC_CTYPE="en_US.UTF-8"
-export LC_MESSAGES="en_US.UTF-8"
+LANGUAGE=en_US.UTF-8
+LANG=en_US.UTF-8
+LC_ALL=en_US.UTF-8
+LC_COLLATE=en_US.UTF-8
+LC_CTYPE=en_US.UTF-8
+LC_MESSAGES=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LC_COLLATE=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_MESSAGES=en_US.UTF-8
 locale-gen en_US.UTF-8
 update-locale en_US.UTF-8
 . /etc/default/locale
