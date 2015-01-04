565e4f3e7ad5c0942ad837cf1d0bf826a3f9d373
M. Omer Golgeli <az@cokh.net>
2015-01-04 22:13:06 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@ cd ~/
 apt-get update
 apt-get -y --force-yes upgrade
 apt-get -y --force-yes install screen nano
-#rm inst.sh* 
-#wget --no-check-certificate https://raw.githubusercontent.com/nomadturk/openresty-install/master/inst.sh
+rm inst.sh* 
+wget --no-check-certificate https://raw.githubusercontent.com/nomadturk/openresty-install/master/inst.sh
 
 
 # Some systems problems with locales. So lets try to add them just to run smoother.
